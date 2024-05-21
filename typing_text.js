cc02ef53f83aff2cef033dd77ad527c4f44cebf6
mogamoga <mogamoga.g1thu6@gmail.com>
2024-05-22 05:32:13 +0900

    史上最高のオンリーワン！

---
+++ typing_text.js
@@ -0,0 +1,16 @@
+
+class TypingText {
+
+    get remainingRomanInput() {
+        // todo
+        return "";
+    }
+
+    isValidInputKey(key) {
+        // todo
+    }
+
+    inputKey(key) {
+        // todo
+    }
+}
