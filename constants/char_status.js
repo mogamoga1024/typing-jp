169e42a7dc755f893b3ed99528224390bd406277
mogamoga <mogamoga.g1thu6@gmail.com>
2024-05-23 03:53:25 +0900

    わくわく叶えるストーリー

---
+++ constants/char_status.js
@@ -0,0 +1,4 @@
+
+export const CHAR_UNMATCH = "unmatch";
+export const CHAR_INCOMPLETE = "incomplete";
+export const CHAR_COMPLETE = "complete";
---
+++ constants/text_status.js
@@ -0,0 +1,4 @@
+
+export const TEXT_UNMATCH = "unmatch";
+export const TEXT_INCOMPLETE = "incomplete";
+export const TEXT_COMPLETE = "complete";
