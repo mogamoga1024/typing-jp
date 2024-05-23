d60559ac67c841861524f0f5ecff286e8f386115
mogamoga <mogamoga.g1thu6@gmail.com>
2024-05-24 07:18:28 +0900

    いーのいーのそのままで

--- sample/sample.js
+++ sample/sample.js
@@ -3,17 +3,23 @@ const domText = document.querySelector("#text");
 const domRoman1 = document.querySelector("#roman1");
 const domRoman2 = document.querySelector("#roman2");
 
-// const text = "こんにちはっす";
-// const text = "Hello, World!";
-// const text = "しゃシゃ";
-// const text = "ヴぁｳﾞｧ";
-// const text = "わたしは「こんにちは」といった。";
-const text = "しゃしゃしゃ";
-// const text = "「なまこ」とは、いったいなんなのか？";
-// const text = "っ";
-// const text = "今日は";
-domText.innerText = text;
-const typingText = new TypingText(text);
+const originalTextList = [
+    "あのイーハトーヴォのすきとおった風、",
+    "夏でも底に冷たさをもつ青いそら、",
+    "うつくしい森で飾られたモリーオ市、",
+    "郊外のぎらぎらひかる草の波。",
+];
+
+const typingTextList = [
+    new TypingText("あのイーハトーヴォのすきとおったかぜ、"),
+    new TypingText("なつでもそこにつめたさをもつあおいそら、"),
+    new TypingText("うつくしいもりでかざられたモリーオし、"),
+    new TypingText("こうがいのぎらぎらひかるくさのなみ。"),
+];
+
+let index = 0;
+domText.innerText = originalTextList[index];
+let typingText = typingTextList[index];
 
 domRoman2.innerText = typingText.remainingRoman;
 
@@ -22,19 +28,44 @@ window.onkeydown = function(e) {
         return;
     }
 
+    // ShiftやF11のような入力を弾く
     if (!TypingText.isValidInputKey(e.key)) {
         return;
     }
 
     const isCapsLock = e.getModifierState("CapsLock");
 
+    // キー入力の更新
     const result = typingText.inputKey(e.key, isCapsLock);
-    if (result === "unmatch") {
-        return;
-    }
-    domRoman1.innerText += e.key;
-    domRoman2.innerText = typingText.remainingRoman;
-    if (result === "complete") {
-        window.onkeydown = null;
+    
+    switch (result) {
+        // 不一致の場合
+        case "unmatch":
+            return;
+
+        // 一致しているが文章が未完成の場合
+        case "incomplete":
+            domRoman1.innerText += e.key;
+            domRoman2.innerText = typingText.remainingRoman;
+            return;
+
+        // 文章が完成の場合
+        case "complete":
+            // クリアしたか
+            if (++index >= originalTextList.length) {
+                domRoman1.innerText += e.key;
+                domRoman2.innerText = typingText.remainingRoman;
+                window.onkeydown = null;
+                return;
+            }
+
+            // 次の文章へ
+            domText.innerText = originalTextList[index];
+            typingText = typingTextList[index];
+        
+            domRoman1.innerText = "";
+            domRoman2.innerText = typingText.remainingRoman;
+
+            return;
     }
 };
