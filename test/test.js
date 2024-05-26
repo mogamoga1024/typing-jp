06ffdf869b102ce72834e9590d3a4d46be728265
mogamoga <mogamoga.g1thu6@gmail.com>
2024-05-27 04:42:28 +0900

    test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,11 @@
+import { TypingText } from "../src/typing_text.js";
+import { EmptyTextError } from "../src/error/empty_text_error.js";
+import { NoRemainingInputError } from "../src/error/no_remaining_input_error.js";
+import { CharCreationError } from "../src/error/char_creation_error.js";
+import { TEXT_UNMATCH, TEXT_INCOMPLETE, TEXT_COMPLETE } from "../src/constants/text_status.js";
 
-test("HOGEHOGE", function() {
-    strictEqual(100, 100);
+test("TypingText", function() {
+    const typingText = new TypingText("てすとですよん");
+
+    strictEqual(typingText.remainingRoman, "tesutodesuyonn");
 });
