4a1f6fc4e23638e7331722d99059191509ec2fe9
mogamoga <mogamoga.g1thu6@gmail.com>
2024-06-09 08:43:52 +0900

    わんだほーい

--- test/test2.js
+++ test/test2.js
@@ -60,4 +60,125 @@ test("じ j", () => {
     strictEqual(typingText.remainingText, "じ");
 });
 
+test("じ i", () => {
+    const priority = {
+        "じ": ["i"]
+    };
+
+    const typingText = new TypingText("じ", priority);
+
+    deepEqual(typingText.char.expectRomanArray, ["zi", "ji"]);
+
+    strictEqual(typingText.roman, "zi");
+    strictEqual(typingText.completedRoman, "");
+    strictEqual(typingText.remainingRoman, "zi");
+
+    strictEqual(typingText.text, "じ");
+    strictEqual(typingText.completedText, "");
+    strictEqual(typingText.remainingText, "じ");
+});
+
+test("じ じゃ j", () => {
+    const priority = {
+        "じゃ": ["j"]
+    };
+
+    const typingText = new TypingText("じ", priority);
+
+    deepEqual(typingText.char.expectRomanArray, ["zi", "ji"]);
+
+    strictEqual(typingText.roman, "zi");
+    strictEqual(typingText.completedRoman, "");
+    strictEqual(typingText.remainingRoman, "zi");
+
+    strictEqual(typingText.text, "じ");
+    strictEqual(typingText.completedText, "");
+    strictEqual(typingText.remainingText, "じ");
+});
+
+test("じ j じゃ", () => {
+    const priority = {
+        "じ": ["j"]
+    };
+
+    const typingText = new TypingText("じゃ", priority);
+
+    strictEqual(typingText.roman, "zya");
+    strictEqual(typingText.completedRoman, "");
+    strictEqual(typingText.remainingRoman, "zya");
+
+    strictEqual(typingText.text, "じゃ");
+    strictEqual(typingText.completedText, "");
+    strictEqual(typingText.remainingText, "じゃ");
+});
+
+test("じ j じゃ j", () => {
+    const priority = {
+        "じ": ["j"]
+    };
+
+    const typingText = new TypingText("じゃ", priority);
+
+    strictEqual(typingText.inputKey("j"), TEXT_INCOMPLETE);
+
+    strictEqual(typingText.roman, "zya");
+    strictEqual(typingText.completedRoman, "j");
+    strictEqual(typingText.remainingRoman, "a");
+
+    strictEqual(typingText.text, "じゃ");
+    strictEqual(typingText.completedText, "");
+    strictEqual(typingText.remainingText, "じゃ");
+});
+
+test("か く c", () => {
+    const priority = {
+        "か": ["c"],
+        "く": ["c"],
+    };
+
+    const typingText = new TypingText("あかあくあ", priority);
+
+    strictEqual(typingText.roman, "acaacua");
+    strictEqual(typingText.completedRoman, "");
+    strictEqual(typingText.remainingRoman, "acaacua");
+
+    strictEqual(typingText.text, "あかあくあ");
+    strictEqual(typingText.completedText, "");
+    strictEqual(typingText.remainingText, "あかあくあ");
+});
+
+test("しゃ ゃ l", () => {
+    const priority = {
+        "ゃ": ["l"]
+    };
+
+    const typingText = new TypingText("しゃ", priority);
+
+    strictEqual(typingText.inputKey("s"), TEXT_INCOMPLETE);
+    strictEqual(typingText.inputKey("i"), TEXT_INCOMPLETE);
+
+    strictEqual(typingText.roman, "sya");
+    strictEqual(typingText.completedRoman, "si");
+    strictEqual(typingText.remainingRoman, "lya");
+
+    strictEqual(typingText.text, "しゃ");
+    strictEqual(typingText.completedText, "し");
+    strictEqual(typingText.remainingText, "ゃ");
+});
+
+test("っゃ ゃ l", () => {
+    const priority = {
+        "ゃ": ["l"]
+    };
+
+    const typingText = new TypingText("っゃ", priority);
+
+    strictEqual(typingText.roman, "llya");
+    strictEqual(typingText.completedRoman, "");
+    strictEqual(typingText.remainingRoman, "llya");
+
+    strictEqual(typingText.text, "っゃ");
+    strictEqual(typingText.completedText, "");
+    strictEqual(typingText.remainingText, "っゃ");
+});
 
