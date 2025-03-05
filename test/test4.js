51b0a09ea6f50524e533da4a2ce3bbc1a83c1cb1
mogamoga <mogamoga.g1thu6@gmail.com>
2025-03-05 19:47:48 +0900

    わんだほい・・・

--- test/test4.js
+++ test/test4.js
@@ -6,6 +6,7 @@ import { TEXT_UNMATCH, TEXT_INCOMPLETE, TEXT_COMPLETE } from "../src/constants/t
 
 QUnit.module("CapsLock");
 
+// todo 見直し
 test("わんだほーい！ WANDAHO-I! CapsLock", () => {
     const typingText = new TypingText("わんだほーい！");
 
@@ -23,6 +24,7 @@ test("わんだほーい！ WANDAHO-I! CapsLock", () => {
     strictEqual(typingText.remainingText, "");
 });
 
+// todo 見直し
 test("わんだほーい！ WANdAho-I! CapsLock", () => {
     const typingText = new TypingText("わんだほーい！");
 
@@ -40,18 +42,105 @@ test("わんだほーい！ WANdAho-I! CapsLock", () => {
     strictEqual(typingText.remainingText, "");
 });
 
-test("にゃ nYA CapsLock", () => {
+test("あいうえお AIUEO CapsLock:true", () => {
+    const typingText = new TypingText("あいうえお");
+
+    for (const key of "AIUE") {
+        strictEqual(typingText.inputKey(key, true), TEXT_INCOMPLETE);
+    }
+    strictEqual(typingText.inputKey("O", true), TEXT_COMPLETE);
+
+    strictEqual(typingText.roman, "aiueo");
+    strictEqual(typingText.completedRoman, "AIUEO");
+    strictEqual(typingText.remainingRoman, "");
+
+    strictEqual(typingText.text, "あいうえお");
+    strictEqual(typingText.completedText, "あいうえお");
+    strictEqual(typingText.remainingText, "");
+});
+
+test("あいうえお a CapsLock:true", () => {
+    const typingText = new TypingText("あいうえお");
+
+    strictEqual(typingText.inputKey("a", true), TEXT_UNMATCH);
+
+    strictEqual(typingText.roman, "aiueo");
+    strictEqual(typingText.completedRoman, "");
+    strictEqual(typingText.remainingRoman, "aiueo");
+
+    strictEqual(typingText.text, "あいうえお");
+    strictEqual(typingText.completedText, "");
+    strictEqual(typingText.remainingText, "あいうえお");
+});
+
+test("あいうえお A CapsLock:false", () => {
+    const typingText = new TypingText("あいうえお");
+
+    strictEqual(typingText.inputKey("A", false), TEXT_UNMATCH);
+
+    strictEqual(typingText.roman, "aiueo");
+    strictEqual(typingText.completedRoman, "");
+    strictEqual(typingText.remainingRoman, "aiueo");
+
+    strictEqual(typingText.text, "あいうえお");
+    strictEqual(typingText.completedText, "");
+    strictEqual(typingText.remainingText, "あいうえお");
+});
+
+test("にゃ n CapsLock:true", () => {
+    const typingText = new TypingText("にゃ");
+
+    strictEqual(typingText.inputKey("n", true), TEXT_UNMATCH);
+
+    strictEqual(typingText.roman, "nya");
+    strictEqual(typingText.completedRoman, "");
+    strictEqual(typingText.remainingRoman, "nya");
+
+    strictEqual(typingText.text, "にゃ");
+    strictEqual(typingText.completedText, "");
+    strictEqual(typingText.remainingText, "にゃ");
+});
+
+test("にゃ N CapsLock:false", () => {
+    const typingText = new TypingText("にゃ");
+
+    strictEqual(typingText.inputKey("N", false), TEXT_UNMATCH);
+
+    strictEqual(typingText.roman, "nya");
+    strictEqual(typingText.completedRoman, "");
+    strictEqual(typingText.remainingRoman, "nya");
+
+    strictEqual(typingText.text, "にゃ");
+    strictEqual(typingText.completedText, "");
+    strictEqual(typingText.remainingText, "にゃ");
+});
+
+test("にゃ Ny CapsLock:true", () => {
+    const typingText = new TypingText("にゃ");
+
+    strictEqual(typingText.inputKey("N", true), TEXT_INCOMPLETE);
+    strictEqual(typingText.inputKey("y", true), TEXT_UNMATCH);
+
+    strictEqual(typingText.roman, "nya");
+    strictEqual(typingText.completedRoman, "N");
+    strictEqual(typingText.remainingRoman, "ya");
+
+    strictEqual(typingText.text, "にゃ");
+    strictEqual(typingText.completedText, "");
+    strictEqual(typingText.remainingText, "にゃ");
+});
+
+test("にゃ nY CapsLock:false", () => {
     const typingText = new TypingText("にゃ");
 
     strictEqual(typingText.inputKey("n", true), TEXT_INCOMPLETE);
-    strictEqual(typingText.inputKey("Y", true), TEXT_INCOMPLETE);
-    strictEqual(typingText.inputKey("A", true), TEXT_COMPLETE);
+    strictEqual(typingText.inputKey("Y", true), TEXT_UNMATCH);
 
     strictEqual(typingText.roman, "nya");
-    strictEqual(typingText.completedRoman, "nYA");
-    strictEqual(typingText.remainingRoman, "");
+    strictEqual(typingText.completedRoman, "n");
+    strictEqual(typingText.remainingRoman, "ya");
 
     strictEqual(typingText.text, "にゃ");
-    strictEqual(typingText.completedText, "にゃ");
-    strictEqual(typingText.remainingText, "");
+    strictEqual(typingText.completedText, "");
+    strictEqual(typingText.remainingText, "にゃ");
 });
